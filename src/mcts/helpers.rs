5f3b0a29d87bda4b09e560a867457c68e1dc7cc1
Shawn Xu <xu107288696@gmail.com>
2025-04-29 00:07:27 -0700

    take 2
    bench 3433651

--- src/mcts/helpers.rs
+++ src/mcts/helpers.rs
@@ -72,7 +72,7 @@ impl SearchHelpers {
     /// #### Note
     /// Must return a value in [0, 1].
     pub fn get_fpu(node: &Node) -> f32 {
-        (1.0 - node.q()).powf(1.3)
+        (1.0 - node.q()).powf(0.95)
     }
 
     /// Get a predicted win probability for an action
