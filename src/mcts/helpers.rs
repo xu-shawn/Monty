cc584e4647e41aedce8ff7e4b85a437f352708bc
Shawn Xu <xu107288696@gmail.com>
2025-04-29 00:38:55 -0700

    take 7
    bench 1741056

--- src/mcts/helpers.rs
+++ src/mcts/helpers.rs
@@ -63,7 +63,7 @@ impl SearchHelpers {
         let scalar = q - q.min(params.winning_pst_threshold());
         let t = scalar / (1.0 - params.winning_pst_threshold());
         let base_pst = 1.0 - params.base_pst_adjustment()
-            + (-1.1 * (depth as f32 - 1.68)).exp();
+            + (-1.1 * (depth as f32 - 1.5)).exp();
         base_pst + (params.winning_pst_max() - base_pst) * t
     }
 
